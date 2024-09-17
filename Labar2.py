307566fa50bd9f3eaf050c915cb8de5d28fb5c4d
Profisionaller <129650632+Profisionaller@users.noreply.github.com>
2024-09-17 21:14:20 +0300

    Update Labar2.py

--- Labar2.py
+++ Labar2.py
@@ -1,4 +1,4 @@
-a= "illin"
+a= "Illin"
 b = "Oleksandr"
 c = 17
 list1=[a, b, c]
@@ -6,8 +6,8 @@ print(list1)
 list2=[type(a), type(b), type(c)]
 print(list2)
 if type(a) == type(b) == type(c):
-    print("всі типт str")
-elif type(a) == type(b) and type(b) == type(c) and type(a) == type(c):
-    print("Тип str, повторюється найчастіше")
-else:
-    print("Один з типів даних не збігається")
+    print("Всі типи str")
+elif type(a) == type(b) !=type(c):
+    print("Переважають тип str ")
+else: 
+    print("Переважно тип int")
