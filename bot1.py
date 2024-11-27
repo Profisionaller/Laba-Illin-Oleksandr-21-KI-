0681811a86518e86ea89aaece3e6c96bd4b47c4e
Profisionaller <129650632+Profisionaller@users.noreply.github.com>
2024-11-27 20:44:31 +0200

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,27 +1,7 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
- 
-token = "7639450810:AAFAaYyjO5q6UDwB8ixcFrlo_vmBvCDuuD8"
-
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Напиши мені щось не будь")
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
-    await update.message.reply_text(text)
-
-
-def main():
-
-    application = ApplicationBuilder().token(token).build()
-
-
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-
-    application.run_polling()
-
-if __name__ == "__main__":
-    main()
+import telebot
+API_TOKEN = '7639450810:AAFAaYyjO5q6UDwB8ixcFrlo_vmBvCDuuD8'
+bot = telebot.TeleBot(API_TOKEN)
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+bot.infinity_polling()
