e05a84ceaf9935bcfaad9b7113947cf6c82bbeb4
Profisionaller <129650632+Profisionaller@users.noreply.github.com>
2024-12-09 20:26:41 +0200

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,7 +1,11 @@
 import telebot
 API_TOKEN = '7639450810:AAFAaYyjO5q6UDwB8ixcFrlo_vmBvCDuuD8'
 bot = telebot.TeleBot(API_TOKEN)
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привіт! Це бот!")
+@bot.message_handler(func=lambda _: True)
+def echo(message):
     bot.reply_to(message, message.text)
+
 bot.infinity_polling()
